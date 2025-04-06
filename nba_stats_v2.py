6d134f3bae8e09dc7f2a043897e0c847eb8b662c
Blaink22 <v4lenj4u@gmail.com>
2025-04-06 05:53:06 -0300

    Delete nba_stats_v2.py

--- nba_stats_v2.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(page_title="NBA Stats Analyzer", layout="wide")
-
-st.title("NBA Stats Analyzer v2")
-
-st.write("Sitio actualizado y funcionando correctamente con todas las funcionalidades.")
